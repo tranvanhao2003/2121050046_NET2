32f451ae15c2087b45444cb9c34ac8b026aa36cf
TranVanHao <143237733+tranvanhao2003@users.noreply.github.com>
2023-10-07 09:48:19 +0700

    Create

---
+++ MVC/CreateMVC/CreateMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CreateMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MVC/CreateMVC/CreateMVC/Views/Person/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CreateMVC.Views.Person
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
